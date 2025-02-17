cead90ea1d2fec556f87b49a8cf412206c611ff5
Erick Gomez <erick@finly.tech>
2025-02-17 14:49:02 -0600

    Add .oh-my-zsh/custom/01_exports.zsh Add .oh-my-zsh/custom/02_aliases.zsh Update .zshrc

---
+++ dot_oh-my-zsh/custom/01_exports.zsh
@@ -0,0 +1,39 @@
+# XDG config
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_STATE_HOME=$HOME/.local/state
+export XDG_CONFIG_DIRS=/etc/xdg
+export XDG_DATA_DIRS=/usr/local/share:/usr/share
+export XDG_CONFIG_DIRS=/etc/xdg
+
+
+# Term colors
+export COLORTERM=truecolor
+export TERM=xterm-256color
+
+# TMUX
+export ZSH_TMUX_AUTOSTART=true
+
+
+#Buildkit
+export COMPOSE_DOCKER_CLI_BUILD=1
+export DOCKER_BUILDKIT=1
+
+#vi-mode
+export VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
+export VI_MODE_SET_CURSOR=true
+
+#editors
+export EDITOR="nvim"
+export VISUAL=${EDITOR}
+export GIT_EDITOR=${EDITOR}
+
+#bat
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
+#batdiff
+export BATDIFF_USE_DELTA=true
+
+#fzf
+export FZF_DEFAULT_COMMAND='fd'
