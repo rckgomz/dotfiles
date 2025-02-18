bf7a058c91ae578312617f1066d860b52e585b52
Erick Gomez <erick@finly.tech>
2025-02-18 00:02:34 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/git-blame.lua Add .config/nvim/lua/plugins/lualline.lua Add .config/nvim/lua/plugins/tmux.lua Add .config/nvim/lua/plugins/vimux.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,61 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- -- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- tmux
+-- Move to window using the <ctrl> hjkl keys
+vim.keymap.set(
+  "n",
+  "<C-h>",
+  ":lua require'tmux'.move_left()<cr>",
+  { silent = true, desc = "Go to left window", remap = true }
+)
+vim.keymap.set(
+  "n",
+  "<C-j>",
+  ":lua require'tmux'.move_bottom()<cr>",
+  { silent = true, desc = "Go to lower window", remap = true }
+)
+vim.keymap.set(
+  "n",
+  "<C-k>",
+  ":lua require'tmux'.move_top()<cr>",
+  { silent = true, desc = "Go to upper window", remap = true }
+)
+vim.keymap.set(
+  "n",
+  "<C-l>",
+  ":lua require'tmux'.move_right()<cr>",
+  { silent = true, desc = "Go to right window", remap = true }
+)
+
+-- Risize to window using the <ctrl> hjkl keys
+vim.keymap.set(
+  "n",
+  "<M-h>",
+  ":lua require'tmux'.resize_left()<cr>",
+  { silent = true, desc = "Go to left window", remap = true }
+)
+vim.keymap.set(
+  "n",
+  "<M-j>",
+  ":lua require'tmux'.resize_bottom()<cr>",
+  { silent = true, desc = "Go to lower window", remap = true }
+)
+vim.keymap.set(
+  "n",
+  "<M-k>",
+  ":lua require'tmux'.resize_top()<cr>",
+  { silent = true, desc = "Go to upper window", remap = true }
+)
+vim.keymap.set(
+  "n",
+  "<M-l>",
+  ":lua require'tmux'.resize_right()<cr>",
+  { silent = true, desc = "Go to right window", remap = true }
+)
+
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr><esc>", { desc = "Safe buffer", noremap = true, silent = true })
