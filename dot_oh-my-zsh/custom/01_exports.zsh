2cad829aeb3e1aaebc309c98f9708dc4778964a7
Erick Gomez <erick@finly.tech>
2025-02-21 23:39:05 -0600

    Update .oh-my-zsh/custom/01_exports.zsh

--- dot_oh-my-zsh/custom/01_exports.zsh
+++ dot_oh-my-zsh/custom/01_exports.zsh
@@ -7,6 +7,7 @@ export XDG_CONFIG_DIRS=/etc/xdg
 export XDG_DATA_DIRS=/usr/local/share:/usr/share
 export XDG_CONFIG_DIRS=/etc/xdg
 
+export PATH="${HOME}/.local/bin/:${PATH}"
 
 # Term colors
 export COLORTERM=truecolor
