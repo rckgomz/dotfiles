ac78fc0f1b2bc8ed11360121ef36c4d019439aa5
Erick Gomez <erick@finly.tech>
2025-02-22 22:24:09 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/colorschema.lua

--- dot_config/nvim/lua/plugins/colorschema.lua
+++ dot_config/nvim/lua/plugins/colorschema.lua
@@ -1,79 +1,78 @@
 return {
-	-- add gruvbox
-	{
-		"catppuccin/nvim",
-		opts = {
-			flavor = "mocha",
-			transparent_background = true,
-			custom_highlights = function(colors)
-				return {
-					-- Set border colors to mauve
-					FloatBorder = { fg = colors.mauve },
-					TelescopeBorder = { fg = colors.mauve },
-					NeoTreeBorder = { fg = colors.mauve },
-					WhichKeyBorder = { fg = colors.mauve },
-					NotifyBorder = { fg = colors.mauve },
-					NoiceCmdlinePopupBorder = { fg = colors.mauve },
-					LazyNormal = { bg = colors.base },
-				}
-			end,
-			integrations = {
-				aerial = true,
-				alpha = true,
-				cmp = true,
-				dashboard = true,
-				flash = true,
-				fzf = true,
-				grug_far = true,
-				gitsigns = true,
-				headlines = true,
-				illuminate = true,
-				indent_blankline = { enabled = true },
-				leap = true,
-				lsp_trouble = true,
-				mason = true,
-				markdown = true,
-				mini = true,
-				native_lsp = {
-					enabled = true,
-					underlines = {
-						errors = { "undercurl" },
-						hints = { "undercurl" },
-						warnings = { "undercurl" },
-						information = { "undercurl" },
-					},
-				},
-				navic = { enabled = true, custom_bg = "lualine" },
-				neotest = true,
-				neotree = true,
-				noice = true,
-				notify = true,
-				semantic_tokens = true,
-				snacks = true,
-				telescope = true,
-				treesitter = true,
-				treesitter_context = true,
-				which_key = true,
-			},
-		},
-		specs = {
-			{
-				"akinsho/bufferline.nvim",
-				optional = true,
-				opts = function(_, opts)
-					if (vim.g.colors_name or ""):find("catppuccin") then
-						opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-					end
-				end,
-			},
-		},
-	},
+  -- add gruvbox
+  {
+    "catppuccin/nvim",
+    opts = {
+      flavor = "mocha",
+      custom_highlights = function(colors)
+        return {
+          -- Set border colors to mauve
+          FloatBorder = { fg = colors.mauve },
+          TelescopeBorder = { fg = colors.mauve },
+          NeoTreeBorder = { fg = colors.mauve },
+          WhichKeyBorder = { fg = colors.mauve },
+          NotifyBorder = { fg = colors.mauve },
+          NoiceCmdlinePopupBorder = { fg = colors.mauve },
+          LazyNormal = { bg = colors.base },
+        }
+      end,
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        fzf = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        snacks = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+          end
+        end,
+      },
+    },
+  },
 
-	-- Configure LazyVim to load gruvbox
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "catppuccin",
-		},
-	},
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
 }
