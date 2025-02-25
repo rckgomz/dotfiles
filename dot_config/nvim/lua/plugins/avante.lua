844ec9d00cbb08df02eab270ea48ac2c0f0c7d6d
Erick Gomez <erick@finly.tech>
2025-02-25 08:48:38 -0600

    feat: configure avante

--- dot_config/nvim/lua/plugins/avante.lua
+++ dot_config/nvim/lua/plugins/avante.lua
@@ -17,7 +17,7 @@ return {
         reasoning_effort = "high", -- only supported for "o" models
       },
       behaviour = {
-        auto_suggestions = true, -- Experimental stage
+        auto_suggestions = false, -- Experimental stage
         auto_set_highlight_group = true,
         auto_set_keymaps = true,
         auto_apply_diff_after_generation = false,
--- dot_config/nvim/lua/plugins/blink.cmp.lua
+++ dot_config/nvim/lua/plugins/blink.cmp.lua
@@ -7,9 +7,43 @@ return {
     },
     opts = function(_, opts)
       opts = {
+        snippets = {
+          expand = function(snippet, _)
+            return LazyVim.cmp.expand(snippet)
+          end,
+        },
+        appearance = {
+          -- sets the fallback highlight groups to nvim-cmp's highlight groups
+          -- useful for when your theme doesn't support blink.cmp
+          -- will be removed in a future release, assuming themes add support
+          use_nvim_cmp_as_default = false,
+          -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+          -- adjusts spacing to ensure icons are aligned
+          nerd_font_variant = "mono",
+        },
+        completion = {
+          accept = {
+            -- experimental auto-brackets support
+            auto_brackets = {
+              enabled = true,
+            },
+          },
+          menu = {
+            draw = {
+              treesitter = { "lsp" },
+            },
+          },
+          documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 200,
+          },
+          ghost_text = {
+            enabled = vim.g.ai_cmp,
+          },
+        },
         sources = {
           -- Add 'avante' to the list
-          default = { "avante", "lsp", "path", "buffer", "snippets" },
+          default = { "avante", "lsp", "path", "buffer", "snippets", "omni" },
           providers = {
             avante = {
               module = "blink-cmp-avante",
@@ -20,8 +54,11 @@ return {
             },
           },
         },
+        cmdline = {
+          enabled = false,
+        },
         keymap = {
-          preset = "super-tab",
+          preset = "enter",
           ["<C-y"] = { "select_and_accept" },
           ["<C-k>"] = { "select_prev", "fallback" },
           ["<C-j>"] = { "select_next", "fallback" },
