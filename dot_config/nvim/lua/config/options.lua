6be675a95d90d6b7548ee616fd50bd51bff02bea
Erick Gomez <erick@finly.tech>
2025-03-01 00:26:36 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/config/options.lua Update .config/nvim/lua/plugins/avante.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -5,3 +5,5 @@ vim.opt.colorcolumn = "80"
 -- views can only be fully collapsed with the global statusline
 vim.opt.laststatus = 3
 vim.o.termguicolors = true
+-- Set to false to disable auto format
+vim.g.lazyvim_eslint_auto_format = true
